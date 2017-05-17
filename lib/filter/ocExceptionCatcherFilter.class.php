b5686857b2bd6f36781c42586c44baf0d4fd3df6
GlennCavarle <glenn.cavarle@gmail.com>
2017-05-17 13:23:10 +0200

    add exception filter to return http status and a json response

---
+++ lib/filter/ocExceptionCatcherFilter.class.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ocExceptionCatcherFilter
+ *
+ * @author Glenn Cavarlé <glenn.cavarle@libre-informatique.fr>
+ */
+class ocExceptionCatcherFilter
+{
+
+    public function execute($filterChain)
+    {
+        try {
+           
+            $filterChain->execute();
+            
+        } catch ( ocAuthCredentialsException $e ) {
+            $r = $this->getResponse();
+            $r->setStatusCode(ApiHttpStatus::UNAUTHORIZED);
+            $r->setContent(json_encode(['message' => $e->getMessage()], JSON_PRETTY_PRINT) . "\n");
+        } catch ( ocException $e ) {
+            $r = $this->getResponse();
+            $r->setStatusCode(ApiHttpStatus::SERVICE_UNAVAILABLE);
+            $r->setContent(json_encode(['message' => $e->getMessage()], JSON_PRETTY_PRINT) . "\n");
+        }
+    }
+
+    private function getResponse()
+    {
+        $response = sfContext::getInstance()->getResponse();
+        if ( null === $response ) {
+            $response = new sfWebResponse(sfContext::getInstance()->getEventDispatcher());
+            sfContext::getInstance()->setResponse($response);
+        }
+        $response->setHttpHeader('Content-type', 'application/json');
+        return $response;
+    }
+}
