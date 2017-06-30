8cec1372bdb65e6476c801045742a86a8c35a542
Fabrice-li <fabrice.petrequin@libre-informatique.fr>
2017-06-30 17:43:04 +0200

    (api) - Fix a bug where multiple ocPro are created (#21)

--- lib/service/ApiCustomersService.class.php
+++ lib/service/ApiCustomersService.class.php
@@ -86,8 +86,14 @@ class ApiCustomersService extends ApiEntityService
 
                 $transaction = $token->OcTransaction;
 
-                if ( !$transaction->oc_professional_id ) {
+                $oc_pro = Doctrine::getTable('OcProfessional')->createQuery('ocp')
+                  ->andWhere('ocp.professional_id = ?', $pro->id)
+                  ->fetchOne();
+
+                if ( !$oc_pro ) {
                     $transaction->OcProfessional = new OcProfessional;
+                } else {
+                  $transaction->OcProfessional = $oc_pro;
                 }
 
                 $transaction->OcProfessional->Professional = $pro;
